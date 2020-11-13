fa24947303af1a3a33300a314a59981a97e47a3b
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-11-12 18:00:39 -0600

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,15 @@
+// A Counter that increases by 1 each second
+// Plus and Minus buttons that increment or decrement the counter
+// A 'like' button (❤️) that adds a 'like' for the number that is currently displayed by the timer
+// A comment box that adds comments when submitted
+
+document.addEventListener("DOMContentLoaded", () => {
+    const counter = document.getElementById('counter')
+    const plus = document.getElementById('plus')
+    const minus = document.getElementById('minus')
+    const heart = document.getElementById('heart')
+    const comment = document.getElementById('comment')
+    const pause = document.getElementById('pause')
+
+    if
+});
